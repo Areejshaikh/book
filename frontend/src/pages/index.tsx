e45091f564fe234019aa99c79e0ca0de81c0d529
Areej <areejzaheer96@gmail.com>
2025-12-15 00:13:17 +0500

    Fix: Deploy configuration for GitHub Pages and enhance homepage

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,31 +1,28 @@
 import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
+
+// Import the available homepage components
+import HeroSection from '@site/src/components/Homepage/HeroSection';
+import ModulesSection from '@site/src/components/Homepage/ModulesSection';
+import WhyThisMattersSection from '@site/src/components/Homepage/WhyThisMattersSection';
+import StartWhereYouAreSection from '@site/src/components/Homepage/StartWhereYouAreSection';
+import FinalCTASection from '@site/src/components/Homepage/FinalCTASection';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into the meta tag">
+      title={`Welcome to ${siteConfig.title}`}
+      description={siteConfig.tagline}>
+      <HeroSection />
       <main>
-        <div className="container margin-vert--xl">
-          <div className="row">
-            <div className="col col--6 col--offset-3">
-              <h1 className="hero__title">{siteConfig.title}</h1>
-              <p className="hero__subtitle">{siteConfig.tagline}</p>
-              <div className="margin-vert--lg">
-                <Link
-                  className="button button--primary button--lg"
-                  to="/docs/intro">
-                  Get Started
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
+        <WhyThisMattersSection />
+        <HomepageFeatures />
+        <ModulesSection />
+        <StartWhereYouAreSection />
+        <FinalCTASection />
       </main>
     </Layout>
   );
